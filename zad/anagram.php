54e41231508f5fc2d12756aeb68d5fb5b79d5db9
Kamil Baraniak <90936580+kbaraniak@users.noreply.github.com>
2023-11-22 16:56:40 +0100

    Update count_char function

--- zad/anagram.php
+++ zad/anagram.php
@@ -4,11 +4,12 @@ if(isset($_POST["form"])){
     $word1 = $_POST["slowo1"];
     $word2 = $_POST["slowo2"];
 
-    if(strlen($word1) == strlen($word2) && count_chars($word1, 3) == count_chars($word2, 3)){
+    if(strlen($word1) == strlen($word2) && count_chars($word1) == count_chars($word2)){
         echo "<p style='color: green;'>Słowo jest anagramem</p>";
     }
     else{
         echo "<p style='color: red;'>Słowo nie jest anagramem</p>";
     }
+
 }
 ?>
